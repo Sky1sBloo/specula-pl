b5a7871f0ebcf583ca12242f668535abc8d5f9ed
Sky1sBloo <ultimate8gaming@gmail.com>
2025-11-17 21:47:31 +0800

    Added documentation to headers

--- src/LexicalAnalyzer/LexerFileReader.hpp
+++ src/LexicalAnalyzer/LexerFileReader.hpp
@@ -3,6 +3,10 @@
 #include "LexicalAnalyzer.hpp"
 #include <string>
 
+/**
+ * Mainly used for reading the file for the lexer
+ * Automatically builds tokens on construct
+ */
 class LexerFileReader {
 public:
     LexerFileReader(LexicalAnalyzer& lexer, const std::string& filePath);
